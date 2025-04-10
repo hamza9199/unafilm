ecb9136172fe78f24f182df9b08c6433d355a0bf
Hamza Gačić <hamza.gacic.22@size.ba>
2025-04-10 23:00:19 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ const PORT = 3000;
 
 // CORS opcije
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['https://una-film-klijent.vercel.app/', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
