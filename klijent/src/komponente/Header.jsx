9000f0b2d1cd6bf726ed5a522a71fd1b5fe31f16
Hamza Gačić <hamza.gacic.22@size.ba>
2025-04-25 01:44:48 +0200

    fix

--- klijent/src/komponente/Header.jsx
+++ klijent/src/komponente/Header.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './css/Header.module.css';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import logo from './../assets/unaFilm141-2.png'; // Adjust the path as necessary
 import { FaSearch } from 'react-icons/fa'; // Na vrhu fajla
-import DropdownSubmenu from './DropdownSubmenu';
-
 
 const Header = () => {
     const [isSearchOpen, setIsSearchOpen] = useState(false);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
     const history = useNavigate();  // Using history to navigate programmatically
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  
+
+useEffect(() => {
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+}, []);
 
     const toggleSearch = (e) => {
         e.preventDefault();
         setIsSearchOpen(!isSearchOpen);
+    
     };
 
     const toggleMobileMenu = () => {
@@ -61,6 +71,8 @@ const Header = () => {
                         </li>
                         <li className={styles.menuItem}><a href="/kontakt">Kontakt</a></li>
                         <li className={styles.searchIcon}>
+                            {!isMobileMenuOpen ? (
+                                <>
                             <a href="#" role="button" aria-label="Search" onClick={toggleSearch}>
                                 <svg width="20" height="20" viewBox="0 0 24 24" className={styles.searchIconSvg} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
@@ -80,6 +92,29 @@ const Header = () => {
                                     </div>
                                 </form>
                             </div>
+                             </>
+    ) : (
+         <>
+                            <a href="#" role="button" aria-label="Search" onClick={toggleSearch}> 
+                                <svg width="20" height="20" viewBox="0 0 24 24" className={styles.searchIconSvg} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                                </svg>
+                            </a>
+                            {isMobile && (
+                            <div className={`${styles.searchBox2} ${styles.open}`}>
+                                <form onSubmit={handleSearchSubmit}>
+                                    <input
+                                        type="text"
+                                        placeholder="Pretraži..."
+                                        value={searchTerm}
+                                        onChange={handleSearchInputChange}
+                                        className={styles.searchInput2}
+                                    />
+                                </form>
+                            </div>
+                        )}
+                            </>
+                             )}
                         </li>
                     </ul>
                 </nav>
